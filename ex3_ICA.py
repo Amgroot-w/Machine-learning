d33ccf0390d6531fec45baebe0a2cc66a7376f73
Zhang Jingchuan <57850924+Amgroot-w@users.noreply.github.com>
2020-12-08 22:50:32 +0800

    update ex3 ICA

---
+++ ex3_ICA.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import FastICA
+C = 200
+x = np.arange(C)
+a = np.linspace(-2, 2, 25)
+s1 = np.array([a, a, a, a, a, a, a, a]).reshape(200, )
+s2 = 2 * np.sin(0.02 * np.pi * x)
+s3 = np.array(20 * (5 * [2] + 5 * [-2]))
+ax1 = plt.subplot(311)
+ax2 = plt.subplot(312)
+ax3 = plt.subplot(313)
+ax1.plot(x,s1)
+ax2.plot(x,s2)
+ax3.plot(x,s3)
+plt.show()
+
+s=np.array([s1,s2,s3])
+ran=2*np.random.random([6,3])
+mix=ran.dot(s)
+ax1 = plt.subplot(321)
+ax2 = plt.subplot(322)
+ax3 = plt.subplot(323)
+ax4 = plt.subplot(324)
+ax5 = plt.subplot(325)
+ax6 = plt.subplot(326)
+ax1.plot(x,mix[0,:])
+ax2.plot(x,mix[1,:])
+ax3.plot(x,mix[2,:])
+ax4.plot(x,mix[3,:])
+ax5.plot(x,mix[4,:])
+ax6.plot(x,mix[5,:])
+plt.show()
+
+ica = FastICA(n_components=3)
+mix = mix.T
+u = ica.fit_transform(mix)
+u = u.T
+ax1 = plt.subplot(311)
+ax2 = plt.subplot(312)
+ax3 = plt.subplot(313)
+ax1.plot(x,u[0,:])
+ax2.plot(x,u[1,:])
+ax3.plot(x,u[2,:])
+plt.show()
